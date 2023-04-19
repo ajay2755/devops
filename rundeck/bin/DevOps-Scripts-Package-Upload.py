315cf3317adc976b98d44d53fdff3909e7c0408a
ajay2755 <126853291+ajay2755@users.noreply.github.com>
2023-04-19 18:22:31 +0800

    Update DevOps-Scripts-Package-Upload.py

--- rundeck/bin/DevOps-Scripts-Package-Upload.py
+++ rundeck/bin/DevOps-Scripts-Package-Upload.py
@@ -5,6 +5,7 @@ import yaml
 import shutil
 import sys
 import os
+import glob
 from zipfile import ZipFile
 
 #Global variables
@@ -12,23 +13,26 @@ x = datetime.datetime.now()
 date_time = x.strftime("%y%m%d%H%M%S")
 zip_filename = 'devops_project_export_' + date_time + '.zip'
 repo_name = "devops"
-branch = 'main'
+branch = 'master'
 username = 'devops'
 artifactory_api_key = ''
+dir_name = 'devops_' + date_time
 
 def get_config_values(input_item):
     #Open config files to read values based on keys.
     file_path = os.path.relpath("../config/config.yml")
     with open(file_path, "r") as stream:
         config = yaml.safe_load(stream)['rundeck']
-        print("extracted value:",config[input_item])
         return config[input_item]
         
 def git_clone(git_token):
     #clone git devops repo in local workspace
-    git_cmd = 'git -c http.sslVerify=false clone --single-branch --branch '+ branch +' https://'+ username +':'+ git_token +'@gitlab.sparksystems.sg/spark-systems/devops.git'
-    os.system(git_cmd)
-
+    os.system('mkdir '+ dir_name)
+    git_clone_url = get_config_values('git_clone_url')
+    git_cmd = 'git -c http.sslVerify=false clone --single-branch --branch '+ branch +' https://'+ username +':'+ git_token +'@' + git_clone_url
+    os.system('cd '+ dir_name +' && '+git_cmd)
+    os.system('cd '+ dir_name +'/'+ repo_name + ' && rm -rf .git')
+    
 def artifactory_file_upload():
     #Upload package to artifactory
     with open(zip_filename, 'rb') as f:
@@ -42,33 +46,32 @@ def artifactory_file_upload():
     if response.status_code == 201:
         print("File successfully uploaded::",response.status_code)
         print(response.text)
-        #Remove zip file from local workspace once it is uploaded in artifactory
-        if os.path.exists(zip_filename):
-            os.remove(zip_filename)
     else:
+        clean_workspace()
         print("upload failed ::",response.status_code)
-        print(response.text)
+        raise Exception((response.text)
         
 def create_package():
     #Create ZIP package for devops scrips
-	with ZipFile(zip_filename, 'w') as zf:
-	    for dirname, subdirs, files in os.walk(repo_name):
-		    zf.write(dirname)
-		    for filename in files:
-			    zf.write(os.path.join(dirname, filename))
-    
+    with ZipFile(zip_filename, 'w') as zf:
+        for dirname, subdirs, files in os.walk(dir_name+'/'+repo_name):
+            x = dirname[19:]
+            zf.write(dirname,x)
+            for filename in files:
+                zf.write(os.path.join(dirname, filename),os.path.join(x, filename))
+                   
 def clean_workspace():	
     #Clean/remove files and directories from local workspace
-    if os.path.exists(repo_name):
-        os.system('rm -rf '+ repo_name)
-    else:
-        print('No directory for cleanup')
-
+    if os.path.exists(dir_name):
+        os.system('rm -rf '+ dir_name)
+        
+    if os.path.exists(zip_filename):
+        os.system('rm -f '+ zip_filename)
+        
 if __name__ == "__main__":
     if len(sys.argv) >= 3:
         artifactory_api_key = sys.argv[1]
         git_token = sys.argv[2]
-        clean_workspace()
         git_clone(git_token)
         create_package()
         artifactory_file_upload()
